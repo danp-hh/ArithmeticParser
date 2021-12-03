cb04d515c98bfe9178ebc19042cd0e79622e6427
Dano P <hell_danno@hotmail.com>
2021-12-03 18:06:58 +0100

    Added Project and TestProject

---
+++ ArithmeticParser.UnitTests/ExpressionInterpreterTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace ArithmeticParser.UnitTests
+{
+    public class Tests
+    {
+        [Test]
+        public void ParseExpression_SingleVariable_AddedToVariableList()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ ArithmeticParser/ExpressionInterpreter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArithmeticParser
+{
+    class ExpressionInterpreter
+    {
+    }
+}

e2953ce4fa46d9fed5579c8018c4240534a2abb4
Dano P <hell_danno@hotmail.com>
2021-12-06 21:51:58 +0100

    Added ShuntingYardAlgorithm and parsing of substring to Output and Operator List
    (cherry picked from commit 752c59998cf2351ac54aee1170ac514138b0b746)

--- ArithmeticParser/ExpressionInterpreter.cs
+++ ArithmeticParser/ExpressionInterpreter.cs
@@ -19,7 +19,6 @@ namespace ArithmeticParser
         public ExpressionInterpreter(string expressionAsText)
         {
             // VariableParser vp = new VariableParser(expressionAsText);
-            _variableList.Add(new Variable('x'), 1);
 
         }
     }

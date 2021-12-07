02a27cfa6148a4d4440e19ce387984d472f1a8ab
Dano P <hell_danno@hotmail.com>
2021-12-07 22:55:38 +0100

    BinaryTreeParser for InifixExpression (ShuntingYard) with Unit Tests

---
+++ ArithmeticParser.UnitTests/BinaryTreeParserTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ArithmeticParser.UnitTests
+{
+    public class BinaryTreeParserTests
+    {
+
+        [TestCase("12+", 3)]
+        [TestCase("12*", 2)]
+        [TestCase("12-", -1)]
+        public void ParseInfixNotation_TwoNumbersAndOneOperator_ExpectedValue(string expression, double expectedResult)
+        {
+            BinaryTreeParser bnt = new BinaryTreeParser();
+            double result = bnt.parseInfixNotation(expression);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase("123*+", 7)]
+        public void ParseInfixNotation_ThreeNumbersAndTwoOperators_ExpectedValue(string expression, double expectedResult)
+        {
+            BinaryTreeParser bnt = new BinaryTreeParser();
+            double result = bnt.parseInfixNotation(expression);
+
+            Assert.AreEqual(expectedResult, result);
+
+        }
+    }
+}
